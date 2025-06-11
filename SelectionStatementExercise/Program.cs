6db20734c6659916039e212e6c3feee8d029bd06
Brian Yates <briany735@gmail.com>
2025-06-11 10:02:26 -0700

    Created if else excercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 7;
+            Console.WriteLine("Guess my favorite number.");
+            int userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+                {
+                Console.WriteLine("You entered an incorrect input. Higher!");
+                }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("You entered an incorrect input. Lower!");
+            }
+            else (userInput == favNumber)
+                
+            {
+                Console.WriteLine("You guessed this number!");
+            }
+                
+
         }
     }
 }
